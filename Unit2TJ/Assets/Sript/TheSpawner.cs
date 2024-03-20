00b9e6b5c9d5c67af993fc2ffe28f033cd3614dd
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2024-03-20 08:23:16 -0700

    Part 4
    Done with 3

--- Unit2TJ/Assets/Sript/TheSpawner.cs
+++ Unit2TJ/Assets/Sript/TheSpawner.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class TheSpawner : MonoBehaviour
 {
     public GameObject[] animalPrefab;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+            
+    }
+     
+    void SpawnRandomAnimal()
+    { 
+   
+       int animalIndex = Random.Range(0, animalPrefab.Length);
+       Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+       Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+  
     }
 }
